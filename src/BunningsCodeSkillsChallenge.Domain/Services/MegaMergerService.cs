2e35601246c415fdd32628b9421fd151a8638b47
chrisdupuy-dev <christopherdupuy@mindfulabstraction.com>
2021-02-13 14:24:26 +1100

    Adding more test scenarios to merge and now checks for conflicting SKUs

--- src/BunningsCodeSkillsChallenge.Domain/Services/MegaMergerService.cs
+++ src/BunningsCodeSkillsChallenge.Domain/Services/MegaMergerService.cs
@@ -1,12 +1,21 @@
 ﻿namespace BunningsCodeSkillsChallenge.Domain.Services
 {
+    using System;
     using Interfaces.Services;
     using Models;
     using System.Collections.Generic;
     using System.Linq;
+    using Microsoft.Extensions.Logging;
 
     public class MegaMergerService : IMegaMergerService
     {
+        private readonly ILogger _logger;
+
+        public MegaMergerService(ILogger<MegaMergerService> logger)
+        {
+            _logger = logger;
+        }
+
         public CommonCatalog GetCommonCatalog(IEnumerable<Company> companies)
         {
             var commonCatalogItems = new List<CommonCatalogItem>();
@@ -17,20 +26,22 @@
                 var otherCompanies = companies.Where(_ => _ != company);
 
                 // Iterate each product in this company
-                var supplierProductBarcodes = company.SupplierProductBarcodes.GroupBy(_ => _.SKU);
-                foreach (var supplierProductBarcode in supplierProductBarcodes)
+                var groupedSupplierProductBarcodes = company.SupplierProductBarcodes.GroupBy(_ => _.SKU);
+                foreach (var groupSupplierProductBarcode in groupedSupplierProductBarcodes)
                 {
                     // Skip this product if already in the system due to previous company (assuming company name is unique) 
-                    if (skusTooIgnore.Contains(company.Name.GetHashCode() ^ supplierProductBarcode.Key.GetHashCode()))
+                    if (skusTooIgnore.Contains(company.Name.GetHashCode() ^ groupSupplierProductBarcode.Key.GetHashCode()))
                         continue;
 
-                    var description = company.Catalogs.First(_ => _.SKU == supplierProductBarcode.Key).Description;
-                    commonCatalogItems.Add(new CommonCatalogItem(supplierProductBarcode.Key, description, company.Name));
+                    var description = company.Catalogs.First(_ => _.SKU == groupSupplierProductBarcode.Key).Description;
+                    commonCatalogItems.Add(new CommonCatalogItem(groupSupplierProductBarcode.Key, description, company.Name));
 
-                    foreach (var barcode in supplierProductBarcode.Select(_ => _.Barcode))
+                    foreach (var barcode in groupSupplierProductBarcode.Select(_ => _.Barcode))
                     {
                         foreach (var otherCompany in otherCompanies)
                         {
+                            CheckForConflictingSku(otherCompany, groupSupplierProductBarcode.Key, barcode);
+
                             // Iterate other companies products looking for matching barcodes to determine if matching products exist
                             // so that duplicates are not added to the catalog, could be improved by stopping when matching SKU is found
                             // for a barcode
@@ -45,6 +56,13 @@
             }
 
             return new CommonCatalog(commonCatalogItems);
+        }
+
+        private void CheckForConflictingSku(Company otherCompany, string sku, string barcode)
+        {
+            var sameSkuBarcodes = otherCompany.SupplierProductBarcodes.Where(_ => _.SKU == sku);
+            if (sameSkuBarcodes.Any() && !sameSkuBarcodes.Any(_ => _.Barcode == barcode))
+                throw new Exception("Conflicting SKUs found");
         }
     }
 }
--- src/BunningsCodeSkillsChallenge.UnitTests/Services/MegaMergerServiceTests.cs
+++ src/BunningsCodeSkillsChallenge.UnitTests/Services/MegaMergerServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿namespace BunningsCodeSkillsChallenge.UnitTests.Services
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using Domain.Models;
+    using Domain.Models.Entities;
     using Domain.Services;
     using Xunit;
 
@@ -23,7 +25,7 @@
             var companyA = csvContextService.ImportCompany("A", SuppliersALocation, CatalogALocation, BarcodesALocation);
             var companyB = csvContextService.ImportCompany("B", SuppliersBLocation, CatalogBLocation, BarcodesBLocation);
 
-            var megaMergerService = new MegaMergerService();
+            var megaMergerService = new MegaMergerService(null);
 
             // Act
             var commonCatalog = megaMergerService.GetCommonCatalog(new[] {companyA, companyB});
@@ -39,6 +41,163 @@
             AssertCommonCatalogExists(commonCatalogItems, "167-eol-949", "Cheese - Grana Padano", "A");
             AssertCommonCatalogExists(commonCatalogItems, "999-epd-782", "Carbonated Water - Lemon Lime", "B");
             AssertCommonCatalogExists(commonCatalogItems, "650-epd-782", "Carbonated Water - Lemon Lime", "A");
+        }
+
+        [Fact]
+        public void GetCommonCatalog_WhenNoBarcodesForItem_ShouldNotAddToCatalog()
+        {
+            // Arrange
+            var expectedSku = "987-xyz-321";
+            var expectedDescription = "4x2 Timber";
+            var expectedSource = "B";
+
+            var suppliersA = new List<Supplier>() {new Supplier() {ID = 1, Name = "SupplierA"}};
+            var catalogsA = new List<Catalog>() {new Catalog() {SKU = "123-abc-789", Description = "2x4 Timber"}};
+            var barcodesA = new List<SupplierProductBarcode>();
+
+            var companyA = new Company("A", catalogsA, barcodesA, suppliersA);
+
+            var suppliersB = new List<Supplier>() { new Supplier() { ID = 2, Name = "SupplierB" } };
+            var catalogsB = new List<Catalog>() { new Catalog() { SKU = expectedSku, Description = expectedDescription } };
+            var barcodesB = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() {SupplierID = 2, SKU = expectedSku, Barcode = "X11111"}
+            };
+
+            var companyB = new Company(expectedSource, catalogsB, barcodesB, suppliersB);
+
+            var megaMergerService = new MegaMergerService(null);
+
+            // Act
+            var commonCatalog = megaMergerService.GetCommonCatalog(new[] {companyA, companyB});
+
+            // Assert
+            Assert.Single(commonCatalog.CommonCatalogItems);
+
+            var commonCatalogItem = commonCatalog.CommonCatalogItems.First();
+            Assert.Equal(expectedSku, commonCatalogItem.SKU);
+            Assert.Equal(expectedDescription, commonCatalogItem.Description);
+            Assert.Equal(expectedSource, commonCatalogItem.Source);
+        }
+
+        [Fact]
+        public void GetCommonCatalog_WhenSkusConflictBetweenCompaniesWithNoMatchingBarcodes_ShouldThrowException()
+        {
+            // Arrange
+            var conflictingSku = "123-abc-789";
+            var description = "2x4 Timber";
+            var sourceA = "A";
+            var sourceB = "B";
+            var barcodeA = "X11111";
+            var barcodeB = "Y11111";
+
+            var suppliersA = new List<Supplier>() { new Supplier() { ID = 1, Name = "SupplierA" } };
+            var catalogsA = new List<Catalog>() { new Catalog() { SKU = conflictingSku, Description = barcodeA } };
+            var barcodesA = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() { SupplierID = 1, SKU = conflictingSku, Barcode = barcodeA }
+            };
+
+            var companyA = new Company(sourceA, catalogsA, barcodesA, suppliersA);
+
+            var suppliersB = new List<Supplier>() { new Supplier() { ID = 2, Name = "SupplierB" } };
+            var catalogsB = new List<Catalog>() { new Catalog() { SKU = conflictingSku, Description = barcodeB } };
+            var barcodesB = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() {SupplierID = 2, SKU = conflictingSku, Barcode = barcodeB}
+            };
+
+            var companyB = new Company(sourceB, catalogsB, barcodesB, suppliersB);
+
+            var megaMergerService = new MegaMergerService(null);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => megaMergerService.GetCommonCatalog(new[] { companyA, companyB }));
+        }
+
+        [Fact]
+        public void GetCommonCatalog_WhenDifferentSkusAndMatchingBarcodes_ShouldAddOnceToCatalog()
+        {
+            // Arrange
+            var expectedSkuA = "123-abc-789";
+            var expectedSkuB = "987-xyz-321";
+            var expectedDescription = "2x4 Timber";
+            var expectedSourceA = "A";
+            var expectedSourceB = "B";
+            var barcode = "X11111";
+
+            var suppliersA = new List<Supplier>() { new Supplier() { ID = 1, Name = "SupplierA" } };
+            var catalogsA = new List<Catalog>() { new Catalog() { SKU = expectedSkuA, Description = expectedDescription } };
+            var barcodesA = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() { SupplierID = 1, SKU = expectedSkuA, Barcode = barcode }
+            };
+
+            var companyA = new Company(expectedSourceA, catalogsA, barcodesA, suppliersA);
+
+            var suppliersB = new List<Supplier>() { new Supplier() { ID = 2, Name = "SupplierB" } };
+            var catalogsB = new List<Catalog>() { new Catalog() { SKU = expectedSkuB, Description = expectedDescription } };
+            var barcodesB = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() {SupplierID = 2, SKU = expectedSkuB, Barcode = barcode}
+            };
+
+            var companyB = new Company(expectedSourceB, catalogsB, barcodesB, suppliersB);
+
+            var megaMergerService = new MegaMergerService(null);
+
+            // Act
+            var commonCatalog = megaMergerService.GetCommonCatalog(new[] { companyA, companyB });
+
+            // Assert
+            Assert.Single(commonCatalog.CommonCatalogItems);
+
+            var commonCatalogItem = commonCatalog.CommonCatalogItems.First();
+            Assert.Equal(expectedSkuA, commonCatalogItem.SKU);
+            Assert.Equal(expectedDescription, commonCatalogItem.Description);
+            Assert.Equal(expectedSourceA, commonCatalogItem.Source);
+        }
+
+        [Fact]
+        public void GetCommonCatalog_WhenMatchingSkuAndBarcode_ShouldAddOnceToCatalog()
+        {
+            // Arrange
+            var expectedSku = "123-abc-789";
+            var expectedDescription = "2x4 Timber";
+            var expectedSourceA = "A";
+            var expectedSourceB = "B";
+            var barcode = "X11111";
+
+            var suppliersA = new List<Supplier>() { new Supplier() { ID = 1, Name = "SupplierA" } };
+            var catalogsA = new List<Catalog>() { new Catalog() { SKU = expectedSku, Description = expectedDescription } };
+            var barcodesA = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() { SupplierID = 1, SKU = expectedSku, Barcode = barcode }
+            };
+
+            var companyA = new Company(expectedSourceA, catalogsA, barcodesA, suppliersA);
+
+            var suppliersB = new List<Supplier>() { new Supplier() { ID = 2, Name = "SupplierB" } };
+            var catalogsB = new List<Catalog>() { new Catalog() { SKU = expectedSku, Description = expectedDescription } };
+            var barcodesB = new List<SupplierProductBarcode>()
+            {
+                new SupplierProductBarcode() {SupplierID = 2, SKU = expectedSku, Barcode = barcode}
+            };
+
+            var companyB = new Company(expectedSourceB, catalogsB, barcodesB, suppliersB);
+
+            var megaMergerService = new MegaMergerService(null);
+
+            // Act
+            var commonCatalog = megaMergerService.GetCommonCatalog(new[] { companyA, companyB });
+
+            // Assert
+            Assert.Single(commonCatalog.CommonCatalogItems);
+
+            var commonCatalogItem = commonCatalog.CommonCatalogItems.First();
+            Assert.Equal(expectedSku, commonCatalogItem.SKU);
+            Assert.Equal(expectedDescription, commonCatalogItem.Description);
+            Assert.Equal(expectedSourceA, commonCatalogItem.Source);
         }
 
         private void AssertCommonCatalogExists(IEnumerable<CommonCatalogItem> commonCatalogItems, string expectedSku, string expectedDescription, string expectedSource)
